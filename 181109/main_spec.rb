a5e198be8cb0a7902b3fdbac722ddc853830022b
Sean C Davis <scdavis41@gmail.com>
2018-11-09 07:24:19 -0500

    Challenge: "Question Marks"

---
+++ 181109/main_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../spec/spec_helper'
+require_relative './main'
+
+describe 'main' do
+
+  it 'returns false when scenario is not found' do
+    %w{aa6?9 aa6???9}.each { |str| expect(pattern_exists?(str)).to eq(false) }
+  end
+
+  it 'returns true when scenario is found' do
+    %w{c?7??sss?3rr1??????5}.each { |str| expect(pattern_exists?(str)).to eq(true) }
+  end
+
+end
