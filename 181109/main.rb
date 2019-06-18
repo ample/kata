5389bf071b18b37f14cd024a9d8915ada594e160
Sean C Davis <scdavis41@gmail.com>
2019-06-18 06:12:42 -0400

    Remove old exercises

--- 181109/main.rb
+++
@@ -1,4 +0,0 @@
-def pattern_exists?(str)
-  matches = str.gsub(/[^0-9\?]/, '').scan(/([0-9]\?\?\?[0-9])/).flatten
-  matches.map { |s| s[0].to_i + s[-1].to_i == 10 }.uniq.eql?([true])
-end
--- 181207/main.rb
+++
@@ -1,9 +0,0 @@
-def vowel_position?(arr)
-  position = arr.map(&:chars)[0..-2].each_with_index.map { |row, x|
-    row[0..-2].each_with_index.map { |col, y|
-      [x,y] if [arr[x][y], arr[x + 1][y], arr[x][y + 1], arr[x + 1][y + 1]]
-        .map { |a| %w{a e i o u}.include?(a) }.uniq.eql?([true])
-    }.compact
-  }.flatten(1).sort_by(&:sum).first
-  position ? position.join('-') : 'not found'
-end
