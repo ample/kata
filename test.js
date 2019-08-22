f8876b8a734545fcd5540171955ce23715b1a4a3
Sean C Davis <scdavis41@gmail.com>
2019-08-22 12:02:48 -0400

    Make testing dynamic

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-require("./1906/test");
-require("./1907/test");
+const glob = require("glob");
+
+const files = glob
+  .sync("./**/test.js")
+  .filter(f => !f.split("/").includes("node_modules"))
+  .filter(f => f.split("/").length > 2);
+
+for (let file of files) require(file);
