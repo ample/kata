6cf653d5530b9ff00022399b170af2374ac79b54
Sean C Davis <scdavis41@gmail.com>
2019-08-22 13:40:58 -0400

    Add prefix to test runner output

--- 1906/test.js
+++ 1906/test.js
@@ -1,71 +1,71 @@
 // ---------------------------------------- | Setup
 
-const chai = require('chai');
-const assert = chai.assert;
+const chai = require('chai')
+const assert = chai.assert
 
 // ---------------------------------------- | Tests
 
 const tests = {
   filterList: function(name, fn) {
-    describe(`${name.toUpperCase()}: filterList()`, () => {
+    describe(`[1906] ${name.toUpperCase()}: filterList()`, () => {
       it('should return an array', () => {
-        assert.typeOf(fn.filterList(['a']), 'array');
-      });
+        assert.typeOf(fn.filterList(['a']), 'array')
+      })
 
       it('should return only numbers', () => {
-        const results = fn.filterList(['a', 1, 2, 'waffles']);
-        results.every(el => assert.typeOf(el, 'number'));
-      });
+        const results = fn.filterList(['a', 1, 2, 'waffles'])
+        results.every(el => assert.typeOf(el, 'number'))
+      })
 
       it('should satisfy test case #1', () => {
-        const results = fn.filterList([1, 2, 'a', 'b']);
-        assert.sameMembers(results, [1, 2]);
-      });
+        const results = fn.filterList([1, 2, 'a', 'b'])
+        assert.sameMembers(results, [1, 2])
+      })
 
       it('should satisfy test case #2', () => {
-        const results = fn.filterList([1, 'a', 'b', 0, 15]);
-        assert.sameMembers(results, [1, 0, 15]);
-      });
+        const results = fn.filterList([1, 'a', 'b', 0, 15])
+        assert.sameMembers(results, [1, 0, 15])
+      })
 
       it('should satisfy test case #3', () => {
-        const results = fn.filterList([1, 2, 'aasf', '1', '123', 123]);
-        assert.sameMembers(results, [1, 2, 123]);
-      });
-    });
+        const results = fn.filterList([1, 2, 'aasf', '1', '123', 123])
+        assert.sameMembers(results, [1, 2, 123])
+      })
+    })
   },
 
   duplicateEncode: function(name, fn) {
-    describe(`${name.toUpperCase()}: duplicateEncode()`, () => {
+    describe(`[1906] ${name.toUpperCase()}: duplicateEncode()`, () => {
       it('should return a string', () => {
-        assert.typeOf(fn.duplicateEncode('din'), 'string');
-      });
+        assert.typeOf(fn.duplicateEncode('din'), 'string')
+      })
 
       it('should satisfy test case #1', () => {
-        assert.equal(fn.duplicateEncode('din'), '(((');
-      });
+        assert.equal(fn.duplicateEncode('din'), '(((')
+      })
 
       it('should satisfy test case #2', () => {
-        assert.equal(fn.duplicateEncode('recede'), '()()()');
-      });
+        assert.equal(fn.duplicateEncode('recede'), '()()()')
+      })
 
       it('should satisfy test case #3', () => {
-        assert.equal(fn.duplicateEncode('Success'), ')())())');
-      });
+        assert.equal(fn.duplicateEncode('Success'), ')())())')
+      })
 
       it('should satisfy test case #4', () => {
-        assert.equal(fn.duplicateEncode('(( @'), '))((');
-      });
-    });
-  }
-};
+        assert.equal(fn.duplicateEncode('(( @'), '))((')
+      })
+    })
+  },
+}
 
 // ---------------------------------------- | Run Tests
 
-const solutions = require('./test-cases');
+const solutions = require('./test-cases')
 
 for (let test of Object.keys(solutions)) {
   for (let dev of solutions[test]) {
-    const fl = require(`./solutions/${dev}`);
-    tests[test](dev, fl);
+    const fl = require(`./solutions/${dev}`)
+    tests[test](dev, fl)
   }
 }
--- 1907/test.js
+++ 1907/test.js
@@ -1,81 +1,81 @@
-const chai = require('chai');
-const glob = require('glob');
-const path = require('path');
+const chai = require('chai')
+const glob = require('glob')
+const path = require('path')
 
-const assert = chai.assert;
+const assert = chai.assert
 
 // ---------------------------------------- | Tests
 
 const tests = {
   createPhoneNumber: function(name, fn) {
-    describe(`${name.toUpperCase()}: createPhoneNumber()`, () => {
+    describe(`[1907] ${name.toUpperCase()}: createPhoneNumber()`, () => {
       it('should satisfy test case #1', () => {
-        const results = fn.createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
-        assert.equal(results, '(123) 456-7890');
-      });
+        const results = fn.createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+        assert.equal(results, '(123) 456-7890')
+      })
 
       it('should satisfy test case #2', () => {
-        const results = fn.createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-        assert.equal(results, '(111) 111-1111');
-      });
+        const results = fn.createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+        assert.equal(results, '(111) 111-1111')
+      })
 
       it('should satisfy test case #3', () => {
-        const results = fn.createPhoneNumber([]);
-        assert.equal(results, false);
-      });
+        const results = fn.createPhoneNumber([])
+        assert.equal(results, false)
+      })
 
       it('should satisfy test case #4', () => {
-        const results = fn.createPhoneNumber([1]);
-        assert.equal(results, false);
-      });
+        const results = fn.createPhoneNumber([1])
+        assert.equal(results, false)
+      })
 
       it('should satisfy test case #5', () => {
-        const results = fn.createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0]);
-        assert.equal(results, false);
-      });
-    });
+        const results = fn.createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0])
+        assert.equal(results, false)
+      })
+    })
   },
 
   positiveSum: function(name, fn) {
-    describe(`${name.toUpperCase()}: positiveSum()`, () => {
+    describe(`[1907] ${name.toUpperCase()}: positiveSum()`, () => {
       it('should return a number', () => {
-        assert.typeOf(fn.positiveSum([1, 2, 3]), 'number');
-      });
+        assert.typeOf(fn.positiveSum([1, 2, 3]), 'number')
+      })
 
       it('should satisfy test case #1', () => {
-        assert.equal(fn.positiveSum([1, 2, 3, 4, 5]), 15);
-      });
+        assert.equal(fn.positiveSum([1, 2, 3, 4, 5]), 15)
+      })
 
       it('should satisfy test case #2', () => {
-        assert.equal(fn.positiveSum([1, -2, 3, 4, 5]), 13);
-      });
+        assert.equal(fn.positiveSum([1, -2, 3, 4, 5]), 13)
+      })
 
       it('should satisfy test case #3', () => {
-        assert.equal(fn.positiveSum([]), 0);
-      });
+        assert.equal(fn.positiveSum([]), 0)
+      })
 
       it('should satisfy test case #4', () => {
-        assert.equal(fn.positiveSum([-1, -2, -3, -4, -5]), 0);
-      });
+        assert.equal(fn.positiveSum([-1, -2, -3, -4, -5]), 0)
+      })
 
       it('should satisfy test case #5', () => {
-        assert.equal(fn.positiveSum([-1, 2, 3, 4, -5]), 9);
-      });
-    });
-  }
-};
+        assert.equal(fn.positiveSum([-1, 2, 3, 4, -5]), 9)
+      })
+    })
+  },
+}
 
 // ---------------------------------------- | Run Tests
 
 const files = glob
   .sync('./1907/solutions/*.js')
   .map(f => path.basename(f))
-  .filter(f => path.basename(f) !== '_default.js');
+  .filter(f => path.basename(f) !== '_default.js')
 
 for (let filename of files) {
   for (let test of Object.keys(tests)) {
-    const fn = require(`./solutions/${filename}`);
-    const dev = filename.replace(path.extname(filename), '');
-    if (fn[test]) tests[test](dev.toUpperCase(), fn);
+    const fn = require(`./solutions/${filename}`)
+    const dev = filename.replace(path.extname(filename), '')
+    if (fn[test]) tests[test](dev.toUpperCase(), fn)
   }
 }
