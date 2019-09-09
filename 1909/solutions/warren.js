ef25b62acd294f2ed5aa36a81dd634f0664894d8
Warren Harrison <warren@hungry-media.com>
2019-09-09 11:44:33 -0400

    Solution to problem 2

--- 1909/solutions/warren.js
+++ 1909/solutions/warren.js
@@ -5,5 +5,13 @@ fn.getAverage = function(nums) {
 };
 
 fn.maxSequence = function(arr) {
-  // ...
+  if (arr.length < 1) { return 0 }
+  let currentMax = arr.reduce((ttl, num) => ttl + num, 0)
+  for (let startIdx = 0; startIdx < arr.length; startIdx++) {
+    for (let endIdx = startIdx; endIdx < arr.length; endIdx++) {
+      testSum = arr.slice(startIdx, endIdx).reduce((ttl, num) => ttl + num, 0)
+      if (testSum > currentMax) { currentMax = testSum }
+    }
+  }
+  return currentMax
 };
