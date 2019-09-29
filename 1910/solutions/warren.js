ba87fc6e50693060363258c9dfca0ea1c77d5472
Warren Harrison <warren@hungry-media.com>
2019-09-29 16:09:27 -0400

    sumOfPairs solution

--- 1910/solutions/warren.js
+++ 1910/solutions/warren.js
@@ -5,5 +5,12 @@ fn.shortestWordLength = function(str) {
 }
 
 fn.sumOfPairs = function(nums, sum) {
-  // ...
+  for (let idx1 = 0; idx1 < nums.length; idx1++) {
+    for (let idx2 = idx1 + 1; idx2 < nums.length; idx2++) {
+      if(nums[idx1] + nums[idx2] == sum) {
+        return [nums[idx1], nums[idx2]]
+      }
+    }
+  }
+  return undefined
 }
