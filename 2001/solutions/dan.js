72ed1c985488c48cc6fe05b37b638ce612a8256b
Dan Holloran <dan@helloample.com>
2019-12-23 16:54:43 -0500

    All tests passing

--- 2001/solutions/dan.js
+++ 2001/solutions/dan.js
@@ -5,28 +5,29 @@ const addEllipses = pagination => {
   const minPage = Math.min(...pagination)
   const maxPage = Math.max(...pagination)
 
-  pagination = [...Array(maxPage).keys()]
-    .map(index => {
-      let pageNumber = index + 1
-      let inBounds = pageNumber > maxPage || pageNumber < minPage
+  return [...Array(maxPage).keys()].map(index => {
+    let pageNumber = index + 1
+    let inBounds = pageNumber > maxPage || pageNumber < minPage
 
-      if (inBounds) {
-        return undefined
-      }
+    if (inBounds) {
+      return undefined
+    }
 
-      if (pagination.includes(pageNumber)) {
-        return pageNumber
-      }
+    if (pagination.includes(pageNumber)) {
+      return pageNumber
+    }
 
-      return '...'
-    })
-    .filter(page => page !== undefined)
-
-  return [...new Set(pagination)]
+    return '...'
+  })
 }
 
 const removeDuplicates = pagination => {
-  return [...new Set(pagination)]
+  let previous
+  return pagination.filter(page => {
+    let isDuplicate = page === previous
+    previous = page
+    return !isDuplicate
+  })
 }
 
 fn.getPaginationPages = function(pageCount, currentPage) {
@@ -54,33 +55,27 @@ fn.getPaginationPages = function(pageCount, currentPage) {
 
   // @todo Never show more than five page numbers.
 
-  let paginationPrevious = [firstPage, previousPage, currentPage]
-  let paginationNext = [nextPage, pageCount]
+  let pagination = [firstPage, previousPage, currentPage, nextPage, pageCount]
 
   // If the current page is the first page, show the next two pages, or if it is the last page, show the previous two pages(assuming those pages exist).
   if (currentPage === firstPage) {
-    paginationPrevious.push(currentPage + 2)
+    pagination.push(currentPage + 2)
   }
 
   if (currentPage === pageCount) {
-    paginationNext.push(currentPage - 2)
+    pagination.push(currentPage - 2)
   }
 
-  paginationPrevious = removeDuplicates(paginationPrevious).sort()
-  paginationNext = removeDuplicates(paginationNext).sort()
-  let pagination = [...addEllipses(paginationPrevious), currentPage, ...addEllipses(paginationNext)]
-
-  pagination = pagination
+  pagination = removeDuplicates(pagination)
     .map(page => {
       const outOfBounds = page > pageCount || page < firstPage
-
       return outOfBounds ? undefined : page
     })
     .sort()
     .filter(page => page !== undefined)
 
-  // Use an ellipses(...) to represent missing pages.
   pagination = addEllipses(pagination, firstPage, pageCount)
+  pagination = removeDuplicates(pagination)
 
   return pagination
 }
