98a067ec54c7af79cf6185a40a7ac0a77b5209e4
Sean C Davis <scdavis41@gmail.com>
2020-01-04 14:48:13 -0500

    [2002/sean] And again

--- 2002/solutions/sean.js
+++ 2002/solutions/sean.js
@@ -4,7 +4,9 @@ const fn = (module.exports = {})
 
 fn.sortBy = function(attr, reverse = false) {
   // console.log(data.filter(x => Object.keys(x).includes(attr)))
+  console.log('1:', data)
   if ([...data].filter(x => Object.keys(x).includes(attr)).length === 0) return [...data]
+  console.log('2:', data)
   return [...data].sort((a, b) => (reverse ? b[attr] > a[attr] : a[attr] > b[attr]))
 }
 
