9931dd49d35077695373cfa75c3a131d4fb187ce
Sean C Davis <scdavis41@gmail.com>
2020-01-04 07:55:50 -0500

    [2002/sean] Solution

---
+++ 2002/solutions/sean.js
@@ -0,0 +1,28 @@
+const data = require('../data.json')
+
+const fn = (module.exports = {})
+
+fn.sortBy = function(attr, reverse = false) {
+  return [...data].sort((a, b) => (reverse ? b[attr] > a[attr] : a[attr] > b[attr]))
+}
+
+fn.getNames = function() {
+  return data
+    .map(x => x.name)
+    .sort()
+    .join(',')
+}
+
+fn.injectNameParts = function() {
+  const getNameParts = name => name.split(' ')
+  const getFirstName = name => getNameParts(name)[0]
+  const getLastName = name => getNameParts(name)[getNameParts(name).length - 1]
+  const getMiddleName = name => (getNameParts(name).length > 2 ? getNameParts(name)[1] : undefined)
+
+  return data.map(x => ({
+    ...x,
+    firstName: getFirstName(x.name),
+    lastName: getLastName(x.name),
+    middleName: getMiddleName(x.name)
+  }))
+}
