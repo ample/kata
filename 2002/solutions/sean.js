dcf44ceeba40af40c521cb00754ee2e396488ee8
Sean C Davis <scdavis41@gmail.com>
2020-01-04 14:50:00 -0500

    [2002/sean] One last time

--- 2002/solutions/sean.js
+++ 2002/solutions/sean.js
@@ -3,15 +3,13 @@ const data = require('../data.json')
 const fn = (module.exports = {})
 
 fn.sortBy = function(attr, reverse = false) {
-  // console.log(data.filter(x => Object.keys(x).includes(attr)))
-  console.log('1:', data)
-  if ([...data].filter(x => Object.keys(x).includes(attr)).length === 0) return [...data]
-  console.log('2:', data)
-  return [...data].sort((a, b) => (reverse ? b[attr] > a[attr] : a[attr] > b[attr]))
+  if (data.filter(x => Object.keys(x).includes(attr)).length === 0) return data
+  let result = [...data].sort((a, b) => (a[attr] > b[attr] ? 1 : -1))
+  return reverse ? result.reverse() : result
 }
 
 fn.getNames = function() {
-  return [...data]
+  return data
     .map(x => x.name)
     .sort()
     .join(',')
@@ -23,7 +21,7 @@ fn.injectNameParts = function() {
   const getLastName = name => getNameParts(name)[getNameParts(name).length - 1]
   const getMiddleName = name => (getNameParts(name).length > 2 ? getNameParts(name)[1] : undefined)
 
-  return [...data].map(x => ({
+  return data.map(x => ({
     ...x,
     firstName: getFirstName(x.name),
     lastName: getLastName(x.name),
