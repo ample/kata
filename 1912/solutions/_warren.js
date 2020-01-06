a28e0a00992636f2e75b7d5b4b0abfa4da49fa98
Warren Harrison <warren@hungry-media.com>
2020-01-06 16:04:38 -0500

    Add humanReadableTime() solution

--- 1912/solutions/_warren.js
+++ 1912/solutions/_warren.js
@@ -1,14 +1,15 @@
 const fn = (module.exports = {})
 
-// - At least six characters
-// - At least one number
-// - At least one lowercase letter
-// - At least one uppercase letter
-
 fn.validatePassword = function(password) {
   return /^(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/.test(password)
 }
 
 fn.humanReadableTime = function(seconds) {
-  // ...
+  let timeSegments = []
+  timeSegments.push(parseInt(seconds / 3600))
+  seconds -= timeSegments[0] * 3600
+  timeSegments.push(parseInt(seconds / 60))
+  seconds -= timeSegments[1] * 60
+  timeSegments.push(seconds)
+  return timeSegments.map(segment => segment.toString().padStart(2, '0')).join(':')
 }
